f3c5e1b3f88dcf416433cbad0d30312f39b7253d
sonjal203 <sonia.jalli77@gmail.com>
2024-08-02 10:39:44 -0400

    Re: Assignment #3 Python 1
    Calculate the BMI using the formula: BMI = Weight/(Height^2)

---
+++ Assignment 3 for Python1 Class.py
@@ -0,0 +1,10 @@
+weight = float(input("Please enter your weight in kg: "))
+height = float(input("Please enter your height in meters: "))
+
+bmi = weight/(height ** 2)
+
+print("Your body mass index(bmi)is: " + str(bmi))
+
+
+
+
