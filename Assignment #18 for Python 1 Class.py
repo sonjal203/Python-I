87e3a6f82d691e74be262f2b121a065820c615a9
sonjal203 <sonia.jalli77@gmail.com>
2024-08-16 15:16:06 -0400

    Re: Assignment #18 for Python 1 Class
    Create a function called concat_tuples that takes two tuples as input and returns a new tuple containing all elements from both input tuples.

---
+++ Assignment #18 for Python 1 Class.py
@@ -0,0 +1,14 @@
+# initialize tuples
+test_tup1 = (1, 3, 5)
+test_tup2 = (4,6)
+
+# printing original tuples
+print("The original tuple 1: " + str(test_tup1))
+print("The original tuple 2: " + str(test_tup2))
+
+# Ways to concatenate tuples
+# using + operator
+res = test_tup1 + test_tup2
+
+#printing result
+print("The tuple after concatenation is: " + str(res))
