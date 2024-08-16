bb0782cd7e703697d67958a91a7b869268fd9657
sonjal203 <sonia.jalli77@gmail.com>
2024-08-16 14:38:23 -0400

    Re: Assignment #17 for Python 1 Class
    Develop a function called find_common_elements that takes two lists as input and returns a new list containing elements that are common to both input lists.

---
+++ Assignment #17 for Python 1 Class.py
@@ -0,0 +1,17 @@
+def common_member(a, b):
+    a_set = set(a)
+    b_set = set(b)
+
+    if (a_set & b_set):
+        print(a_set & b_set)
+    else:
+        print("No common elements")
+
+
+a = [1, 2, 3, 4, 5]
+b = [5, 6, 7, 8, 9]
+common_member(a, b)
+
+a = [1, 2, 3, 4, 5]
+b = [6, 7, 8, 9]
+common_member(a, b)
